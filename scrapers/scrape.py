c7d92fb7e5561ea4b5fedd45874d1c2c94e2f1ce
Frank Bi <fbi@newshour.org>
2014-06-06 17:06:24 -0400

    adding a number to each name

---
+++ scrapers/scrape.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def getMNames(trs, year, w):
+	for tr in trs:
+		td = tr.findAll("td")
+		w.writerow([year, td[0].text, td[1].text, "m"])
+
+def getFNames(trs, year, w):
+	for tr in trs:
+		td = tr.findAll("td")
+		w.writerow([year, td[0].text, td[2].text, "f"])
+
+def request(w, year):
+	URL = "http://www.ssa.gov/cgi-bin/popularnames.cgi"
+	payload = { "year": year, "top": 1000 }
+	r = requests.post(URL, data=payload)
+	soup = BeautifulSoup(r.text)
+	table = soup.find("table", {"width":"$tablewidth"})
+	trs = table.findAll("tr", {"align":"right"})
+	getMNames(trs, year, w)
+	getFNames(trs, year, w)
+
+def init(startYear, endYear):
+	header = ["year","rank","name","gender"]
+	outfile = open("babynames.csv", "wb")
+	writer = csv.writer(outfile)
+	writer.writerow(header)
+	endYear += 1
+	for year in range(startYear, endYear):
+		request(writer, year)
+	outfile.close()
+
+if __name__ == "__main__":
+	init(1880, 2013)
